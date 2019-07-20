70036b2c21b943476ab6f93bdc800b4c280c0250
NealKemissi <neal.k@hotmail.fr>
2019-07-20 18:45:49 +0200

    add many dishes in menu

--- src/app/Admin/Menu/adminDishCreate/adminDishCreate.component.ts
+++ src/app/Admin/Menu/adminDishCreate/adminDishCreate.component.ts
@@ -12,13 +12,15 @@ import { DishesDTO } from "src/app/models/dto/dishesDTO";
 export class AdminDishCreate {
   name: string;
   /***/
-  price:number;
+  price: number;
   /***/
-  dish_type:string;
+  dish_type: string;
   /***/
-  id_dish_type:number;
+  id_dish_type: number;
   /***/
-  id_menu: number;
+  menu_name: string;
+  /***/
+  all_menus_of_restaurant: Menu[] = [];
   /***/
   error: string;
   /***/
@@ -29,10 +31,20 @@ export class AdminDishCreate {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private menuDishService: MenuDishBookingService
+    private menuDishBookingService: MenuDishBookingService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    /** tout les menus disponnibles, si le client veut ajouter des plats à sa commande */
+    this.menuDishBookingService.getAllMenus().subscribe(
+      data => {
+        this.all_menus_of_restaurant = data;
+      },
+      error => {
+        this.error = error;
+      }
+    );
+  }
 
   preventInputPrice() {
     let value = this.price;
@@ -45,69 +57,57 @@ export class AdminDishCreate {
     }
   }
 
-  preventInputMenu() {
-    let value = this.id_menu;
-    if (value >= 100) {
-      event.preventDefault();
-      this.id_menu = parseInt(value.toString().substring(0, 2));
-    } else if (value <= -1) {
-      event.preventDefault();
-      this.id_menu = 0;
+  onCreate() {
+    this.error = undefined;
+    this.msgCreating = true;
+    if (this.dish_type == "Entrée") {
+      this.id_dish_type = 1;
+    } else if (this.dish_type == "Plat") {
+      this.id_dish_type = 2;
+    } else this.id_dish_type = 3;
+
+    if (
+      this.name == undefined ||
+      this.price == undefined ||
+      this.id_dish_type == undefined
+    ) {
+      this.error = "Tous les champs doivent etre remplis !";
+    } else {
+      if (this.menu_name != undefined) {
+        let id_menu : number = this.all_menus_of_restaurant.find(m => m.name == this.menu_name).id;
+        let menu: Menu;
+        this.menuDishBookingService.getMenu(id_menu).subscribe(
+          data => {
+            menu = data;
+            this.createDish(id_menu);
+          },
+          error => {
+            this.error = error;
+            this.msgCreating = false;
+          }
+        );
+      } else {
+        this.createDish(null);
+      }
     }
   }
 
-  onCreate(){
-    this.msgCreating = true;
-      if(this.dish_type == 'Entrée'){
-        this.id_dish_type = 1;
-      } else if(this.dish_type == 'Plat'){
-        this.id_dish_type = 2;
-      } else this.id_dish_type = 3;
-
-      
-
-
-      if(this.name == undefined || this.price == undefined || this.id_dish_type == undefined){
-          this.error = 'Tous les champs doivent etre remplis !'
-      } else {
-        if(this.id_menu != undefined){
-            let menu: Menu;
-            this.menuDishService.getMenu(this.id_menu).subscribe(
-              data => {
-                menu = data;
-                this.createDish(this.id_menu);
-              },
-              error => {
-                this.error = error;
-                this.msgCreating = false;
-              }
-            );
-          } else {
-            this.createDish(null);
-          }
-      }
-  }
-
-  createDish(id_menu: number){    
+  createDish(id_menu: number) {
     this.error = undefined;
-      let dishDTO: DishesDTO = new DishesDTO(
-        null,
-        this.name,
-        this.price,
-        true,
-        this.id_dish_type,
-        id_menu
-      );
-      this.menuDishService
-        .createDish(dishDTO)
-        .subscribe(
-          data => (dishDTO = data),
-          error => (this.error = error)
-        );
-      setTimeout(() => {
-        //requete http create ...
-        this.router.navigate(["/adminMenu"]);
-      }, 2500);
-      
+    let dishDTO: DishesDTO = new DishesDTO(
+      null,
+      this.name,
+      this.price,
+      true,
+      this.id_dish_type,
+      id_menu
+    );
+    this.menuDishBookingService
+      .createDish(dishDTO)
+      .subscribe(data => (dishDTO = data), error => (this.error = error));
+    setTimeout(() => {
+      //requete http create ...
+      this.router.navigate(["/adminMenu"]);
+    }, 2500);
   }
 }
--- src/app/Admin/Menu/adminMenuUpdate/adminMenuUpdate.component.ts
+++ src/app/Admin/Menu/adminMenuUpdate/adminMenuUpdate.component.ts
@@ -18,6 +18,10 @@ export class AdminMenuUpdate {
   /***/
   dishes: Dishes[] = [];
   /***/
+  all_dishes_of_restaurant: Dishes[] = [];
+  /***/
+  dishes_to_add: string[] = [];
+  /***/
   dish: string;
   /***/
   error: string;
@@ -39,12 +43,14 @@ export class AdminMenuUpdate {
           this.menu = data;
           this.menuDishBookingService.getAllDishes().subscribe(
             data => {
-              this.dishes = data;
-              this.menu_detail = this.dishes.filter(
+              this.all_dishes_of_restaurant = data;
+              this.menu_detail = this.all_dishes_of_restaurant.filter(
                 d => d.id_menu == params["id"]
               );
               /** on clean pour laisser visible uniquement les plats dispos*/
-              this.dishes = this.dishes.filter(d => d.id_menu == null);
+              this.dishes = this.all_dishes_of_restaurant.filter(
+                d => d.id_menu == null
+              );
             },
             error => {
               this.error = error;
@@ -61,8 +67,15 @@ export class AdminMenuUpdate {
     });
   }
 
-  revert() {
-    this.dish = undefined;
+  addTempDish(dish_name: string) {
+    if (dish_name != undefined) {
+      this.dishes_to_add.push(dish_name);
+    }
+  }
+
+  deleteTempDish(dish_name: string) {
+    let i = this.dishes_to_add.indexOf(dish_name);
+    this.dishes_to_add.splice(i, 1);
   }
 
   onUpdate() {
@@ -76,36 +89,40 @@ export class AdminMenuUpdate {
       this.menuDishBookingService
         .updateMenu(menuDTO)
         .subscribe(data => (menuDTO = data));
-      this.updateDish();
-      setTimeout(() => {
-        //requete http update ...
-        this.router.navigate(["/adminMenu"]);
-      }, 2500);
+      this.updateAllDishes();
+      this.finish();
     }
   }
 
-  updateDish() {
-    if (this.dish != undefined) {
-      var dish_to_add: Dishes;
-      var all_dishes: Dishes[];
-
-      this.menuDishBookingService.getAllDishes().subscribe(
-        data => {
-          dish_to_add = data.find(d => d.name == this.dish);
-          let dishDTO: DishesDTO = new DishesDTO(
-            dish_to_add.id,
-            dish_to_add.name,
-            dish_to_add.price,
-            dish_to_add.available,
-            dish_to_add.id_dish_type,
-            this.menu.id
-          );
-          this.menuDishBookingService
-            .updateDish(dishDTO)
-            .subscribe(data => (dishDTO = data), error => (this.error = error));
-        },
-        error => (this.error = error)
+  updateAllDishes() {
+    this.dishes_to_add.forEach(dish => {
+      let dish_to_update: Dishes = this.all_dishes_of_restaurant.find(
+        d => d.name == dish
       );
-    }
+      if (dish_to_update != undefined) {
+        this.updateDish(dish_to_update);
+      }
+    });
+  }
+
+  updateDish(dish_to_update: Dishes) {
+    let dishDTO: DishesDTO = new DishesDTO(
+      dish_to_update.id,
+      dish_to_update.name,
+      dish_to_update.price,
+      dish_to_update.available,
+      dish_to_update.id_dish_type,
+      this.menu.id
+    );
+    this.menuDishBookingService
+      .updateDish(dishDTO)
+      .subscribe(data => (dishDTO = data), error => (this.error = error));
+  }
+
+  finish() {
+    setTimeout(() => {
+      //requete http update ...
+      this.router.navigate(["/adminMenu"]);
+    }, 2500);
   }
 }
